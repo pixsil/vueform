f8b778b377daa008df661f114b288604d5c7cdf0
pixsil <57232302+pixsil@users.noreply.github.com>
2021-05-07 10:28:51 +0200

    Update vueForm.js

--- vueForm.js
+++ vueForm.js
@@ -8,6 +8,7 @@ window.VueForm = class VueForm {
      */
     constructor(formData = {}) {
         this.sendJsonFormData = false;
+        this.sendDontSendEmptyValues = true;
         this.originalFormData = {...formData};
         this.formData = formData;
         this.vueErrors = new VueErrors(formData);
@@ -84,7 +85,7 @@ window.VueForm = class VueForm {
         for (let property in this.originalFormData) {
 
             // if null dont sent
-            if (this.formData[property] === null) {
+            if (this.sendDontSendEmptyValues === true && this.formData[property] === null) {
                 continue;
             }
             // if null dont sent
